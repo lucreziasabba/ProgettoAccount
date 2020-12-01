9fe08e531aa77025dd300888ba06cdbb75dd8e78
lucrezia sabba <lucrezia.sabba@icloud.com>
2020-12-01 12:25:12 +0100

    aggiunta cs

--- Acquisti_/MainWindow.xaml.cs
+++ Acquisti_/MainWindow.xaml.cs
@@ -23,6 +23,65 @@ namespace Acquisti_
         public MainWindow()
         {
             InitializeComponent();
+            txtUtente.Focus();
+            txtPrezzo.IsEnabled = false;
+            txtQuantita.IsEnabled = false;
+            cmbProdotto.IsEnabled = false;
+            btnPulisci.IsEnabled = false;
+            btnStampa.IsEnabled = false;
+            btnRimuoviSelezione.IsEnabled = false;
+        }
+        private const string PASS = "QuartaAI";
+        private string utente;
+
+        private void BtnAccedi_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtUtente.Text!=""&& txtPassword.Text!="")
+            {
+                if(txtPassword.Text==PASS)
+                {
+                    string utente = txtUtente.Text;
+
+                    txtUtente.IsEnabled = false;
+                    txtPassword.IsEnabled = false;
+                    btnAccedi.IsEnabled = false;
+
+                    txtPrezzo.IsEnabled = true;
+                    txtQuantita.IsEnabled = true;
+                    cmbProdotto.IsEnabled = true;
+                    btnStampa.IsEnabled = true;
+                    btnPulisci.IsEnabled = true;
+                    btnRimuoviSelezione.IsEnabled = true;
+                    lstSelezione.IsEnabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("PASSWORD ERRATA");
+                }
+            }
+        }
+
+        private void BtnPulisci_Click(object sender, RoutedEventArgs e)
+        {
+            cmbProdotto.SelectedItem = null;
+            txtPrezzo.Text = null;
+            txtQuantita.Text = null;
+        }
+
+        private void BtnRimuoviSelezione_Click(object sender, RoutedEventArgs e)
+        {
+            lstSelezione.Items.RemoveAt(lstSelezione.SelectedIndex);
+        }
+
+        private void BtnStampa_Click(object sender, RoutedEventArgs e)
+        {
+            string item;
+            string prodotto = ((ComboBoxItem)cmbProdotto.SelectedItem).Content.ToString();
+            double prezzo_prodotto = double.Parse(txtPrezzo.Text);
+            int quantita = int.Parse(txtQuantita.Text);
+            double prezzo = prezzo_prodotto * quantita;
+            item = $"l'utente {utente} ha acquistato {quantita} {prodotto} al prezzo di {prezzo}€";
+            lstSelezione.Items.Add(item);
         }
     }
 }
